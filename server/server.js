e491fa452adae3dd6e586e4fe2211aec8fadc606
belus90 <belus90@gmail.com>
2022-04-22 14:20:47 +0100

    there was a bug in server.js in line10

--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const createRouter = require('.helper/create_router');
+// const createRouter = require('.helper/create_router');
 
 app.use(cors());
 app.use(express.json());
 
 const MongoClient = require('mongodb').MongoClient;
-const createRouter = require('.helper/create_router.js');
+const createRouter = require('./helper/create_route.js');
 
-MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: ture})
+MongoClient.connect('mongodb://0.0.0.0:27017', { useUnifiedTopology: true})
 .then((client) => {
-    const db = client.db('shares_portfolio');
+    const db = client.db('Shares_Portfolio');
     const sharesColection = db.collection('shares');
     const sharesRouter = createRouter(sharesColection);
-    app.use('/api/',sharesRouter);
+    app.use('/api/shares',sharesRouter);
 })
 .catch(console.err);
 
