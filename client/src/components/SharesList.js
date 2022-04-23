b92098570c4cb720f0341a9309ca098b983aae54
Max Cardwell <maximillian.cardwell@gmail.com>
2022-04-23 13:05:56 +0100

    Fixed handleclick function

--- client/src/components/SharesList.js
+++ client/src/components/SharesList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SharesItem from "./SharesItem";
 
-const SharesList = ({shares}) => {
+const SharesList = ({shares, handleShareClicked}) => {
     const shareList = shares.map((share, index) => {
         return <SharesItem share = {share} key = {index} handleShareClicked={handleShareClicked}/>
     })
