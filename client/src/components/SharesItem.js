8d68c3c85ea3d6c895547ca9ccc9227fe98c6f8c
belus90 <belus90@gmail.com>
2022-04-23 11:19:29 +0100

    checking db

---
+++ client/src/components/SharesItem.js
@@ -0,0 +1,13 @@
+
+const SharesItems = ({share, index}) => {
+
+    return(
+        <div>
+            {share["Meta Data"]["2. Symbol"]}
+            <p>I am the SharesList </p>
+        </div>
+    );
+
+}
+
+export default SharesItems;
--- client/src/components/SharesList.js
+++ client/src/components/SharesList.js
@@ -1,10 +1,14 @@
+import SharesItems from "./SharesItem";
 
-
-const SharesList = ({}) => {
+const SharesList = ({shares, index}) => {
+    const shareList = shares.map((share) => {
+        return <SharesItems share = {share} index = {index} />
+    })
 
 
     return(
         <div>
+            {shareList}
             <p>I am the SharesList </p>
             </div>
     );
