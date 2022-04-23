ab48d56c5a726b1735bceb191ea0f0c72e801d8d
sofiaIG <user@users-MacBook-Pro.local>
2022-04-23 13:27:29 +0100

    still working on the form. addShare not a function error

--- client/src/components/NewShareForm.js
+++ client/src/components/NewShareForm.js
@@ -1,41 +1,37 @@
-import React from 'react';
 import React, { useState } from 'react';
-import {postShares} from "../components/SharesService";
 
-const NewShareForm = (addShares) => {
-    const [newForm, setNewForm] = useState([])
+const NewShareForm = (addShare) => {
+    const [name, setName] = useState('');
+    const [numberShares, setNumberShares] = useState(0)
 
-    const onChange=(event)=>{
-        newForm[event.target.id] = event.target.value;
-        setNewForm(newForm);
-    }
+    const handleName =event => setName(event);
+    const handleShares = event => setNumberShares(event)
 
-    const onSubmit =(event) =>{
+    const handleSubmit =(event) =>{
         event.preventDefault();
-        postShares(newForm).then((data)=>{   
-            addShares(data);
-        })
+        const shares = {
+            name,
+            numberShares
+        }
+        addShare(shares);
+        setName = ('');
+        setNumberShares = (0)
 
     }
-
-
-
 
     return (
-        <form onSubmit={onSubmit} id = 'shares-form'>
+        <form onSubmit={handleSubmit} id = 'shares-form'>
             <h2>Add a share</h2>
             <div>
                 <label htmlFor='name'>Name of the Company: </label>
-                <input onChange={onChange} type = 'text' id = 'name'/>
+                <input onChange={handleName} type = 'text' id = 'name'/>
             </div>
             <div>
                 <label htmlFor='number-of-shares'>Number of Shares: </label>
-                <input onChange={onChange} type = 'number' min = '1'
-                max = '50' value ='0' id = 'number-of-shares'/>
+                <input onChange={handleShares} type = 'number' min = '1'
+                max = '50' id = 'number-of-shares'/>
             </div>
-
-
-
+            <input type="submit" value="Save" id="save"/>
         </form>
     );
 }
