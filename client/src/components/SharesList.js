debf848cc3c68022d1b39c28abe1b99d872298a4
belus90 <belus90@gmail.com>
2022-04-23 13:27:51 +0100

    ShareItem updated

--- client/src/components/SharesList.js
+++ client/src/components/SharesList.js
@@ -9,7 +9,7 @@ const SharesList = ({shares, index}) => {
     return(
         <div>
             {shareList}
-            <p>I am the SharesList </p>
+            {/* <p>I am the SharesList </p> */}
             </div>
     );
 }
