466e4c4df3d6b323959717c53860962d120a64fc
belus90 <belus90@gmail.com>
2022-04-26 22:05:06 +0100

    main chart almost finished

--- client/src/components/DisplayAll.js
+++ client/src/components/DisplayAll.js
@@ -1,21 +1,61 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS } from 'chart.js/auto';
 import DisplayDetail from "./DisplayDetail";
 
-const DisplayAll = ({data})=>{
-//   const allData = data.data.map(d=>d)
-//   console.log( typeof allData)
-const listOfShare = data.data
-  const shareList = listOfShare.map((share, index) => {
-      
-    return <DisplayDetail key={index} share={share} />
-});
+const DisplayAll = ({ data }) => {
+    //   const allData = data.data.map(d=>d)
+    //   console.log( typeof allData)
+    const listOfShare = data.data
+    const shareList = listOfShare.map((share, index) => {
 
-    return(
-        <div>
-        {shareList}
-        
-        
-        </div>
+        return <DisplayDetail key={index} share={share} />
+    });
+
+    const newArrayWithName = (listOfShare).map((object) => {
+        return object.name
+    })
+    const newArrayWithPrice = (listOfShare).map((object) => {
+        return object.priceUsd
+    })
     
+    // console.log(newArrayWithPrice);
+    
+    const state = {
+        labels: newArrayWithName,
+        datasets: [
+            {
+                label: "Share data diagramm name by price",
+                fill: false,
+                lineTension: 0.7,
+                backgroundColor: 'rgba(75,192,192,1)',
+                borderColor: 'rgba(0,0,0,1)',
+                borderWidth: 1.5,
+                data: newArrayWithPrice,
+
+            }
+        ],
+    }
+
+    return (
+        <div>
+            <div>
+                <Bar
+                    data={state}
+                    width = {500}
+                    height = {200}
+                    options={{
+                            plugins: {
+                                title: {
+                                    display: true,
+                                    text: ""
+                                }
+                            }
+                    }}
+                />
+            </div>
+            {shareList}
+        </div>
     )
 }
 
