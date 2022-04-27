f370e819b5317ced00b6d2ae3afae9f3568bc9bd
belus90 <belus90@gmail.com>
2022-04-27 15:14:02 +0100

    overall chart finished

--- client/src/components/OverallPerformance.js
+++ client/src/components/OverallPerformance.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Pie } from 'react-chartjs-2';
 import { Chart as ChartJS } from 'chart.js/auto';
 
-const OverallPerformance = ({shareNames, shares}) => {
+const OverallPerformance = ({ shareNames, shares }) => {
 
     const getTotalOfShare = (totalHeld, sharePrice) => {
         return parseFloat(totalHeld * sharePrice).toFixed(2);
@@ -14,7 +14,7 @@ const OverallPerformance = ({shareNames, shares}) => {
 
         shares.forEach((object) => {
             shareNames.forEach((dbShare) => {
-                if (object.data.id === dbShare.name){
+                if (object.data.id === dbShare.name) {
                     let total = getTotalOfShare(dbShare.shares_held, object.data.priceUsd)
                     let tempObject = {}
                     tempObject[object.data.id] = total;
@@ -31,72 +31,72 @@ const OverallPerformance = ({shareNames, shares}) => {
         return Object.keys(object)[0]
     });
 
-    const arrayOfNewPrices = arrayOfPrices.map((object)=> {
+    const arrayOfNewPrices = arrayOfPrices.map((object) => {
         return Object.values(object)[0]
     });
 
+    const arrayOfNewPricesNumber = arrayOfNewPrices.map(p => Number(p));
 
+    // const newTotalSum = () => {
+    //     let total = 0;
+    //     arrayOfNewPricesNumber.forEach(x => total += x)
+    //     return total;
+    // }
+
+    // const  percentage = arrayOfNewPricesNumber.map((object) => {
+    //     return ((object / newTotalSum())* 100).toFixed(3)
+    // })
 
     const state = {
         labels: arrayOfLabels,
         datasets: [
-        {
-            label: 'Rainfall',
-            backgroundColor: [
-              'rgb(137,149,199)',
-              'rgb(137,199,152)',
-              'rgb(209,165,102)',
-              'rgb(102,209,191)',
-              'rgb(152,102,209)',
-              //new rgb's
-              'rgb(196,8,8)',
-              'rgb(179,43,115)',
-              'rgb(133,179,43)',
-              'rgb(59,64,50)',
-              'rgb(79,227,165)',
-            
-             
-            ],
-            hoverBackgroundColor: [
-            '#501800',
-            '#4B5000',
-            '#175000',
-           
-            ],
-            data: arrayOfNewPrices
-          }
+            {
+                label: 'Rainfall',
+                backgroundColor: [
+                    'rgb(137,149,199)',
+                    'rgb(137,199,152)',
+                    'rgb(209,165,102)',
+                    'rgb(102,209,191)',
+                    'rgb(152,102,209)',
+                    //new rgb's
+                    'rgb(196,8,8)',
+                    'rgb(179,43,115)',
+                    'rgb(133,179,43)',
+                    'rgb(59,64,50)',
+                    'rgb(79,227,165)',
+
+
+                ],
+                hoverBackgroundColor: [
+                    '#501800',
+                    '#4B5000',
+                    '#175000',
+
+                ],
+                data: arrayOfNewPrices
+            }
         ]
-      }
+    }
 
 
     return (
         <div>
- <h3>Overall Performance</h3>
+            <h3>Overall Performance</h3>
             <div>
-            <Pie
-            data={state}
-            options={{
-                title:{
-                display:true,
-                text:'Average Rainfall per month',
-                fontSize:20
-                },
-                legend:{
-                display:true,
-                position:'right'
-                },
-        //     scales: {
-        //         y: {
-        //             ticks: {
-        //                 // Include a dollar sign in the ticks
-        //                 callback: function(value, index, ticks) {
-        //                     return  value + '%';
-        //                 }
-        //             }
-        //         }
-        //   }
-        }}
-        />
+                <Pie
+                    data={state}
+                    options={{
+                        title: {
+                            display: true,
+                            text: 'Average Rainfall per month',
+                            fontSize: 20
+                        },
+                        legend: {
+                            display: true,
+                            position: 'right'
+                        }
+                    }}
+                />
             </div>
         </div>
     );
