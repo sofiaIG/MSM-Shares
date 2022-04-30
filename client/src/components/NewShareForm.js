8d14cd1c2882d69b44fe0db2a5c35ff34f0c9240
Max Cardwell <maximillian.cardwell@gmail.com>
2022-04-30 13:31:53 +0100

    Fixed the add new share to match needing a buy price

--- client/src/components/NewShareForm.js
+++ client/src/components/NewShareForm.js
@@ -4,22 +4,25 @@ import { postShares } from '../components/SharesService';
 
 const NewShareForm = ({fetchNewShare, handleFormClick}) => {
     const [name, setName] = useState('');
-    const [shares_held, setNumberShares] = useState(0)
+    const [shares_held, setNumberShares] = useState(0);
+    const [buy_price, setBuy_Price] = useState(0);
 
     const handleName =event => setName(event.target.value);
-    const handleShares = event => setNumberShares(event.target.value)
-
+    const handleShares = event => setNumberShares(event.target.value);
+    const handleBuyPrice = event => setBuy_Price(event.target.value);
     
 
     const handleSubmit =(event) =>{
         event.preventDefault();
         const shares = {
             name,
-            shares_held
+            shares_held,
+            buy_price
         }
         fetchNewShare(shares);
         setName('');
         setNumberShares(0);
+        setBuy_Price(0);
         handleFormClick();
     }
 
@@ -34,6 +37,10 @@ const NewShareForm = ({fetchNewShare, handleFormClick}) => {
                 <label htmlFor='shares_held'>Number of Shares: </label>
                 <input onChange={handleShares} value ={shares_held} type = 'number' min = '1'
                 max = '50' id = 'number-of-shares' required/>
+            </div>
+            <div>
+                <label htmlFor='buy_price'>Buy price: </label>
+                <input onChange={handleBuyPrice} value={buy_price} type='number' required id='buy-price'/>
             </div>
             <button type="submit"  id="save">Save</button>
         </form>
